1afe17b54b52aa8765d804bd58a6a5254c1445e4
ddkangfu <ddkangfu@gmail.com>
2014-02-21 14:30:44 +0800

    使用Yii框架初始化了项目代码。

---
+++ protected/yiic.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../../../../../Work/Soruce/Yii/yii-1.1.14.f0fee9/framework/yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
